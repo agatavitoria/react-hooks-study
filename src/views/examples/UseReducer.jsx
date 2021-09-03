144105077ac2aa023ff3dcaf3bbe6d4ddffd1c92
Agata Vitoria <agata-vitoria>
2021-09-02 22:06:34 -0300

    useReducer parte 1

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
+const initialState = {
+    cart: [],
+    products: [],
+    user: null,
+    // foco...
+    number: 0,
+}
+
+function reducer(state, action) {
+    switch(action.type) {
+        case 'numberAdd2':
+            return {...state, number: state.number + 2}
+        case 'login':
+            return { ...state, user: { name: action.payload } }
+        default:
+            return state
+    }
+}
+
 const UseReducer = (props) => {
+    const [state, dispatch] = useReducer(reducer, initialState)
+
     return (
         <div className="UseReducer">
             <PageTitle
                 title="Hook UseReducer"
                 subtitle="Uma outra forma de ter estado em componentes funcionais!"
             />
+            <div className="center">
+                <span className="text">{state.user ? state.user.name : 'Sem Usuário'}</span>
+                <span className="text">{state.number}</span>
+                <button className="btn" onClick={() => dispatch({ type: 'login', payload: 'Maria' })}>Login</button>
+                <button className="btn" onClick={() => dispatch({ type: 'numberAdd2' })}>+2</button>
+            </div>
         </div>
     )
 }
