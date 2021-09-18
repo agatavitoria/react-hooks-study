789e69df365e3805de115bb11806200baa95a096
Agata Vitoria <agata-vitoria>
2021-09-18 12:15:07 -0300

    useReducer desafio

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -12,7 +12,15 @@ const initialState = {
 function reducer(state, action) {
     switch(action.type) {
         case 'numberAdd2':
-            return {...state, number: state.number + 2}
+            return {...state, number: state.number + 2 }
+        case 'numberMulti7':
+            return {...state, number: state.number * 7 }
+        case 'numberDiv25':
+            return {...state, number: state.number / 25 }
+        case 'numberInt':
+            return {...state, number: parseInt(state.number) }
+        case 'numberAddN':
+            return {...state, number: state.number + action.payload }
         case 'login':
             return { ...state, user: { name: action.payload } }
         default:
@@ -34,6 +42,10 @@ const UseReducer = (props) => {
                 <span className="text">{state.number}</span>
                 <button className="btn" onClick={() => dispatch({ type: 'login', payload: 'Maria' })}>Login</button>
                 <button className="btn" onClick={() => dispatch({ type: 'numberAdd2' })}>+2</button>
+                <button className="btn" onClick={() => dispatch({ type: 'numberMulti7' })}>*7</button>
+                <button className="btn" onClick={() => dispatch({ type: 'numberDiv25' })}>/25</button>
+                <button className="btn" onClick={() => dispatch({ type: 'numberInt' })}>Int</button>
+                <button className="btn" onClick={() => dispatch({ type: 'numberAddN', payload: -9 })}>-9</button>
             </div>
         </div>
     )
